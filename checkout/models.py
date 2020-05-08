97c3385684e5c7932726e3a0b02380a00f109afc
Sean Lewis <slewis100@gmail.com>
2020-05-08 14:03:26 +0000

    added checkout views, checkout.html and Stripe JS

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from products.models import Product
-# Create your models here.
 
+# Create your models here.
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
@@ -25,3 +25,4 @@ class OrderLineItem(models.Model):
     def __str__(self):
         return "{0} {1} @ {2}".format(
             self.quantity, self.product.name, self.product.price)
+
